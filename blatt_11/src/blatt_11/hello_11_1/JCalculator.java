2489fb24418cdc85c7356d469e9d0d0c62f41319
OlafKnolaf <ichkaufenichts@gmail.com>
2016-07-02 22:26:20 +0200

    11.1

---
+++ blatt_11/src/blatt_11/hello_11_1/JCalculator.java
@@ -0,0 +1,26 @@
+package blatt_11.hello_11_1;
+
+public class JCalculator {
+	public static int add(int x, int y) {
+		return x + y;
+	}
+
+	public static int sub(int x, int y) {
+		return x - y;
+	}
+
+	public static int mult(int x, int y) {
+		return x * y;
+	}
+
+	public static double div(int x, int y) {
+		if (y == 0) {
+			throw new IllegalArgumentException("Böse 0!");
+		}
+		return (double) x / (double) y;
+	}
+
+	public static int round(double x) {
+		return (int) Math.ceil(x);
+	}
+}
---
+++ blatt_11/src/blatt_11/hello_11_1/main.scala
@@ -0,0 +1,10 @@
+package blatt_11.hello_11_1
+
+object main extends App {
+  println("Calling JCalculator methods...")
+  println(JCalculator.add(3, 4))
+  println(JCalculator.sub(3, 4))
+  println(JCalculator.mult(3, 4))
+  println(JCalculator.div(3, 4))
+  println(JCalculator.round(3.4))
+}
